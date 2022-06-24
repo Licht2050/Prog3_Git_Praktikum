7590d1b97ec8b844dd13ede331df44af4a7ef7b8
sayed.sajadi <ssajadi@htwsaar.de>
2022-06-24 18:09:30 +0200

    init Menue.java

---
+++ Menue.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Menue{
+        public static void main(String[] args) {
+                final int PROGRAMMENDE=0;
+                boolean isEnde=false;
+                Scanner scanner = new Scanner(System.in);
+                while(!isEnde){     
+                        int wahl = scanner.nextInt();
+                        switch (wahl) {
+                                case PROGRAMMENDE:
+                                        System.out.println("Ciao");
+                                        isEnde=true;
+                                break;
+                                case 1:
+                                        System.out.println("Michael Sauer");
+                                break;
+                                case 2:
+                                        //wer da sonst noch so wäre ...
+                                break;
+                                default:
+                                        System.out.println("Fehler: Kenne ich nicht!");
+                        }     
+                }
+                scanner.close();
+        } 
+}
