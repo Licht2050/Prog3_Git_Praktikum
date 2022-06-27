984baa4b1a71b37f53307afcc338921130a4466c
Nick Paul <npaul@eduroam-192-109-116-130.htw-saarland.de>
2022-06-27 12:08:22 +0200

    added Nick

--- Menue.java
+++ Menue.java
@@ -26,6 +26,9 @@ public class Menue{
                                 case 17:
                                         System.out.println("123");
                                 break;
+                                case 69:
+                                        System.out.println("Nick Paul");
+                                break;
                                 default:
                                         System.out.println("Fehler: Kenne ich nicht!");
                         }     
